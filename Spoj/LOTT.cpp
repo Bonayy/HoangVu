87a8e9e06fb2883e64be437ab46455961c37d35e
thepluck <hoangvu24pro@gmail.com>
2020-03-10 13:44:04 +0700

    nani

--- Spoj/LOTT.cpp
+++ Spoj/LOTT.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     string cmd; long double n;
     fixed(cout); cout.precision(15);
     while (cin >> cmd){
