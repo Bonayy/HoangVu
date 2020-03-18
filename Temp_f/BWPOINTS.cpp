c13bef4e6ea822fd31cd1b8ca9ad1c52bb10780b
thepluck <hoangvu24pro@gmail.com>
2020-03-18 13:18:24 +0700

    gg

--- Temp_f/BWPOINTS.cpp
+++
@@ -1,7 +0,0 @@
-Dòng đầu tiên ghi các số nguyên $L_1$, $L_2$, $L_3$, $C_1$, $C_2$, $C_3$ ($1 \le$ $L_1$ $\le$ $L_2$ $\le$ $L_3$ $\le 10^{9}$; $1 \le$ $C_1$ $\le$ $C_2$ $\le$ $C_3$ $\le 10^{9}$) theo đúng thứ tự liệt kê ở trên.
-
-Dòng thứ hai chứa số lượng nhà ga $N$ ($2 \le N \le 100000$)
-
-Dòng thứ ba ghi hai số nguyên $s$, $f$ là các chỉ số của hai nhà ga cần tìm cách đặt mua vé với chi phí nhỏ nhất để đi lại giữa chúng.
-
-Dòng thứ $i$ trong số $N$ - $1$ dòng tiếp theo ghi số nguyên là khoảng cách từ nhà ga $A$ (ga $1$) đến nhà ga thứ $i$ + $1$.
