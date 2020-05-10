6499b0f778c7c5358f6105173d5cb3bdb9b0d29a
thepluck <hoangvu24pro@gmail.com>
2020-05-10 19:36:51 +0700

    dasdsa

--- Data Structure/Trie/emoticons.cpp
+++ Data Structure/Trie/emoticons.cpp
@@ -1,64 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 const int N = 2000;
-char *seq = "!?.,:;-_’#$%&/=*+(){}[]qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
-char s[1000];
-
-struct trie{
-    int nxt[N][128], aut[N][128], link[N], sz; bool leaf[N];
-
+ 
+class trie{
+private:
+    int nxt[N][128], aut[N][128], link[N], near[N], sz; bool leaf[N];
+ 
+public:
     void init(){
         for (int i = 0; i <= sz; i++){
-            link[i] = leaf[i] = 0;
-            for (int j = 0; seq[j]; j++) nxt[i][seq[j]] = aut[i][seq[j]] = 0;
+            link[i] = near[i] = leaf[i] = 0;
+            for (int c = 0; c < 128; c++) nxt[i][c] = aut[i][c] = 0;
         }
         sz = 0;
     }
-
-    void insert(char s[]){
+ 
+    void insert(const string &s){
         int pt = 0;
-        for (int i = 0; s[i]; i++){
-            if (!nxt[pt][s[i]]) nxt[pt][s[i]] = ++sz;
-            pt = nxt[pt][s[i]];
+        for (char c : s){
+            if (!nxt[pt][c]) nxt[pt][c] = ++sz; pt = nxt[pt][c];
         }
         leaf[pt] = 1;
     }
-
-
+ 
+ 
     void build_link(){
         queue <int> q; q.push(0);
         while (!q.empty()){
             int u = q.front(); q.pop();
-            for (int j = 0; seq[j]; j++){
-                if (nxt[u][seq[j]]){
-                    link[nxt[u][seq[j]]] = u ? aut[link[u]][seq[j]] : 0;
-                    q.push(nxt[u][seq[j]]); aut[u][seq[j]] = nxt[u][seq[j]];
-                    leaf[nxt[u][seq[j]]] |= leaf[link[nxt[u][seq[j]]]];
+            for (int c = 0; c < 128; c++){
+                if (nxt[u][c]){
+                    link[nxt[u][c]] = u ? aut[link[u]][c] : 0;
+                    q.push(nxt[u][c]); aut[u][c] = nxt[u][c];
                 }
-                else aut[u][seq[j]] = aut[link[u]][seq[j]];
+                else aut[u][c] = aut[link[u]][c];
             }
         }
+        q.push(0);
+        while (!q.empty()){
+            int u = q.front(); q.pop();
+            for (int c = 0; c < 128; c++)
+                if (nxt[u][c]){
+                    if (leaf[nxt[u][c]]) near[nxt[u][c]] = nxt[u][c];
+                    else near[nxt[u][c]] = near[link[nxt[u][c]]];
+                    q.push(nxt[u][c]);
+                }
+        }
     }
-
-    int solve(char s[]){
+ 
+    int solve(const string &s){
         int pt = 0, res = 0;
-        for (int i = 0; s[i]; i++){
-            pt = aut[pt][s[i]]; if (leaf[pt]){pt = 0; res++;}
+        for (char c : s){
+            pt = aut[pt][c]; if (leaf[near[pt]]) pt = 0, res++;
         }
         return res;
     }
 } tt;
-
+ 
 bool solve(){
-    int n, m; scanf("%d%d", &n, &m);
+    int n, m; cin >> n >> m;
     if (n + m == 0) return false; tt.init();
-    while (n--){gets(s); tt.insert(s);}
-    tt.build_link(); int res = 0;
-    while (m--){gets(s); res += tt.solve(s);}
-    printf("%d\n", res); return true;
+    string s; while (n--){cin >> s; tt.insert(s);}
+    tt.build_link(); int res = 0; cin.ignore();
+    while(m--){getline(cin, s); res += tt.solve(s);}
+    cout << res << '\n'; return true;
 }
-
+ 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     while(solve());
 }
