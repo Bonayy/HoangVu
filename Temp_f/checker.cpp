4fd2b63eef707b933965aa2e28030f6dba94b2c0
Nguyễn Hoàng Vũ <54402720+thepluck@users.noreply.github.com>
2020-06-05 06:29:05 +0700

    df

--- Temp_f/checker.cpp
+++ Temp_f/checker.cpp
@@ -2,21 +2,26 @@
 using namespace std;
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-string task = "boundseq";
+string task = "TREELIS";
+
+int ver[10005];
 
 int main(){
-    for (int i = 1; i <= 100; i++){
+    for (int i = 1; i <= 15; i++){
         stringstream ss; string name;
         ss >> name; name = "test" + name;
         ofstream inp(task + ".inp");
-        int n = size_t(rng()) % 500000;
+        int n = size_t(rng()) % 105;
         inp << n << '\n';
-        for (int i = 1; i <= n; i++)
-            inp << size_t(rng()) % 1000000000 << ' ';
-        inp.close(); system("./solution");
-        system(("./" + task).data());
-        if (system(("diff " + task + ".out " + task + ".ans").data()))
-            return cout << "wrong\n", 0;
+        iota(ver + 1, ver + n + 1, 1);
+        shuffle(ver + 1, ver + n + 1, rng);
+        for (int j = 1; j <= n; j++)
+            inp << size_t(rng()) % 100001 << " \n"[j == n];
+        for (int j = 1; j < n; j++)
+            inp << ver[j] << ' ' << ver[j + 1] << '\n';
+        inp.close(); system("solution"); system(task.data());
+        if (system(("fc " + task + ".out " + task + ".ans").data()))
+            return cout << "wrong at test case " << i << '\n', 0;
     }
     cout << "correct\n";
 }
--- Temp_f/solution.cpp
+++ Temp_f/solution.cpp
@@ -1,36 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 5e5 + 5;
-int l[N], r[N], a[N], maxj, maxi[N];
-vector <int> rev[N]; set <int> s;
+#define eb emplace_back
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ub upper_bound
+#define lb lower_bound
+
+const int N = 1e3 + 5;
+int w[N], d[N], n, res;
+vector <int> adj[N];
+
+void dfs_sol(int u, int p){
+    int k = lb(d + 1, d + n + 1, w[u]) - d;
+    res = max(res, k); swap(d[k], w[u]);
+    for (int v : adj[u])
+        if (v != p) dfs_sol(v, u);
+    swap(d[k], w[u]);
+}
 
 int main(){
-    freopen("boundseq.inp", "r", stdin);
-    freopen("boundseq.ans", "w", stdout);
+    freopen("TREELIS.inp", "r", stdin);
+    freopen("TREELIS.ans", "w", stdout);
     ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n; cin >> n; vector <int> st;
-    for (int i = 1; i <= n; cin >> a[i++]);
-    st.push_back(0); int res = 0;
+    cin.tie(nullptr);
+    cin >> n;
+    for (int i = 1; i <= n; cin >> w[i++]);
+    for (int i = 1, u, v; i < n; i++){
+        cin >> u >> v;
+        adj[u].eb(v); adj[v].eb(u);
+    }
     for (int i = 1; i <= n; i++){
-        while (st.back() && a[st.back()] <= a[i])
-            st.pop_back();
-        l[i] = st.back() + 1; st.push_back(i);
+        memset(d, 0x3f, sizeof d);
+        dfs_sol(i, 0);
     }
-    st.clear(); st.push_back(n + 1);
-    for (int i = n; i > 0; i--){
-        while (st.back() <= n && a[st.back()] >= a[i])
-            st.pop_back();
-        r[i] = st.back() - 1; st.push_back(i);
-    }
-    for (int i = 1; i <= n; i++) rev[l[i]].push_back(i);
-    for (int i = 1; i <= n; i++){
-        for (int x : rev[i]) s.insert(x);
-        auto it = s.upper_bound(r[i]);
-        if (it != s.begin())
-            res = max(res, *(--it) - i + 1);
-    }
-    if (res < 2) cout << "-1\n";
-    else cout << res << '\n';
+    cout << res << '\n';
 }
